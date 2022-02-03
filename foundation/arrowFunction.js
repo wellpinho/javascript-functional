c7871e3747aa77bfdc2124c2cb601c47e2030312
wellpinho <wellpinho@outlook.com>
2022-02-03 19:30:39 -0300

    ✨ NEW FEATURE: function arrow

---
+++ foundation/arrowFunction.js
@@ -0,0 +1,10 @@
+const increment = function(n) {
+  return n + 1
+}
+console.log(increment(2)) // 3
+
+const increment2 = n => n + 1
+console.log(increment2(5)) // 6
+
+const sum = (a, b) => a + b
+console.log(sum(50, 49))
