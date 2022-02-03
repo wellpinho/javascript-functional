d08ef25f1954843a265cd3d58c724ec49037de4f
wellpinho <wellpinho@outlook.com>
2022-02-03 19:20:18 -0300

    ✨ NEW FEATURE: function declaration

---
+++ foundation/anatomy.js
@@ -0,0 +1,30 @@
+// function declaration
+
+function sayHello() {
+  console.log('Hello')
+}
+
+sayHello()
+console.log('*******************************')
+
+function sayHelloTo(name) {
+  console.log(`Hello ${name}`)
+}
+
+sayHelloTo('Wellington')
+console.log('*******************************')
+
+function returnHi() {
+  return 'HI'
+}
+
+const greting = returnHi()
+console.log(greting)
+console.log('*******************************')
+
+function returnHiTo(name) {
+  return `Hi ${name}`
+}
+
+console.log(returnHiTo('Wellington pinho'))
+
